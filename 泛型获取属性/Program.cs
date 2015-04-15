3ad20408892fe806b61b66d3e66179916f8e83ef
starry <245711031@qq.com>
2015-04-15 10:39:12 +0800

    泛型获取属性

---
+++ 泛型获取属性/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private void GetList<T>(T oldValue, T newValue)
+        {
+            T t = Activator.CreateInstance<T>();   
+
+             foreach (System.Reflection.PropertyInfo info in typeof(T).GetProperties())
+                {
+                    object oldPropertyValue = info.GetValue(oldValue, null);
+                    object newPropertyValue = info.GetValue(newValue, null);
+                    if (oldPropertyValue != newPropertyValue)
+                    {
+                        DescriptionAttribute[] arrDesc = (DescriptionAttribute[])info.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                        if (arrDesc == null || arrDesc.Length == 0)
+                        {
+                            continue;
+                        }                     
+                    }
+                }
+        }
+    }
+}
